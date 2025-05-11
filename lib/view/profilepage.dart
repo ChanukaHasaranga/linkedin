1d524f2b4c050021b17923fab150cf2339cdd9d1
nimthara <96354974+ChanukaHasaranga@users.noreply.github.com>
2025-05-12 04:30:54 +0530

    profilepage

--- lib/view/profilepage.dart
+++ lib/view/profilepage.dart
@@ -63,33 +63,87 @@ class _ProfilePageState extends State<ProfilePage>
                                   image: AssetImage("assets/post1.jpg"),
                                   fit: BoxFit.cover)),
                         ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Container(
+                                decoration: const BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: Colors.white),
+                                child: IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(
+                                      Icons.edit,
+                                      color: Colors.blue,
+                                      size: 20,
+                                    )),
+                              ),
+                            ),
+                          ],
+                        ),
                         Positioned(
                           bottom: -50,
-                          left: 20,
+                          left: 15,
                           child: FadeTransition(
                             opacity: _fadeAnimation,
                             child: CircleAvatar(
                               radius: 50,
                               backgroundColor: Colors.grey[300],
-                              backgroundImage: const AssetImage("assets/profile.png"),
+                              backgroundImage:
+                                  const AssetImage("assets/profile.png"),
                             ),
                           ),
                         ),
+                        Positioned(
+                          bottom: -55,
+                          left: 80,
+                          child: Container(
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                      color: const Color.fromARGB(83, 0, 0, 0),
+                                      width: 1),
+                                  color: Colors.white,
+                                  shape: BoxShape.circle),
+                              child: const Padding(
+                                padding: EdgeInsets.all(4.0),
+                                child: Icon(
+                                  Icons.add,
+                                  size: 25,
+                                  color: Colors.black,
+                                ),
+                              )),
+                        )
                       ],
                     ),
-                    const SizedBox(height: 60),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.edit,
+                                color: Colors.black,
+                                size: 30,
+                              )),
+                        ),
+                      ],
+                    ),
                     Padding(
-                      padding: const EdgeInsets.only(left: 25),
+                      padding: const EdgeInsets.only(left: 15),
                       child: Text(
                         user.name ?? 'Unknown Name',
                         style: const TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ),
+                            fontSize: 25,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black),
                       ),
                     ),
                     Padding(
-                      padding: const EdgeInsets.only(left: 25),
+                      padding: const EdgeInsets.only(left: 15),
                       child: Text(
                         user.headline ?? 'No Headline',
                         style: const TextStyle(
@@ -99,7 +153,7 @@ class _ProfilePageState extends State<ProfilePage>
                       ),
                     ),
                     Padding(
-                      padding: const EdgeInsets.only(left: 25),
+                      padding: const EdgeInsets.only(left: 15),
                       child: Text(
                         user.languages ?? 'Not added',
                         style: TextStyle(
@@ -108,46 +162,327 @@ class _ProfilePageState extends State<ProfilePage>
                         ),
                       ),
                     ),
-                    Text(
-                      user.location ?? 'No Location',
-                      style: TextStyle(
-                        fontSize: 14,
-                        color: Colors.grey[600],
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15, top: 10),
+                      child: Text(
+                        (user.work ?? 'No working').toUpperCase(),
+                        style: const TextStyle(
+                          fontSize: 16,
+                          color: Colors.black,
+                        ),
                       ),
                     ),
-                    const SizedBox(height: 16),
-                    Center(
-                      child: ElevatedButton(
-                        onPressed: () {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                              content: Text(
-                                "Edit Profile not implemented",
-                                style: TextStyle(color: Colors.black),
-                              ),
-                              backgroundColor: Colors.white,
-                              behavior: SnackBarBehavior.floating,
-                              margin:
-                                  EdgeInsets.only(top: 10, left: 10, right: 10),
-                              duration: Duration(seconds: 3),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15),
+                      child: Text(
+                        user.location ?? 'No Location',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey[600],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15, top: 10),
+                      child: Row(
+                        children: [
+                          Text(
+                            "${user.followers ?? 00000} followers",
+                            style: const TextStyle(
+                                fontSize: 16,
+                                color: Colors.blue,
+                                fontWeight: FontWeight.w500),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            "${user.connection ?? 00000}+ connections",
+                            style: const TextStyle(
+                                fontSize: 16,
+                                color: Colors.blue,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 10,),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15),
+                      child: Row(children: [
+                        smallbuttons(context, "Open to", Colors.blue,
+                            Colors.white, false),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        smallbuttons(context, "Add section", Colors.white,
+                            Colors.grey, true),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                              color: Colors.transparent,
+                              border: Border.all(color: Colors.grey),
+                              shape: BoxShape.circle),
+                          child: const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Icon(
+                              Icons.more_horiz,
+                              color: Colors.grey,
                             ),
-                          );
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blue,
-                          foregroundColor: Colors.white,
-                          minimumSize: const Size(200, 50),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8),
                           ),
                         ),
-                        child: const Text('Edit Profile'),
+                      ]),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10, left: 15),
+                      child: SizedBox(
+                        height: 115,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 3,
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding: const EdgeInsets.only(right: 8),
+                              child: workcontainer(),
+                            );
+                          },
+                        ),
                       ),
                     ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: Container(
+                        color: const Color(0xFFE5EEF7),
+                        padding: const EdgeInsets.all(16),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Your Dashboard",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                    SizedBox(height: 4),
+                                    Text(
+                                      "Private to you",
+                                      style: TextStyle(
+                                        color: Colors.grey,
+                                        fontSize: 12,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    Icon(Icons.star_border, color: Colors.grey),
+                                    SizedBox(width: 4),
+                                    Text(
+                                      "ALL-STAR",
+                                      style: TextStyle(
+                                        color: Colors.grey,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                            const SizedBox(height: 16),
+                            Container(
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              padding: const EdgeInsets.symmetric(vertical: 16),
+                              child: IntrinsicHeight(
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    _dashboardStat(
+                                        "111", "Who viewed\nyour profile"),
+                                    _verticalDivider(),
+                                    _dashboardStat("500", "Post Views"),
+                                    _verticalDivider(),
+                                    _dashboardStat("85", "Search\napperances"),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 16),
+                            Container(
+                              padding: const EdgeInsets.all(12),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              child: const Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    children: [
+                                      Icon(Icons.wifi_tethering,
+                                          color: Colors.black),
+                                      SizedBox(width: 8),
+                                      Text(
+                                        "Creator mode: On",
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  Text(
+                                    "You are a ios developer",
+                                    style: TextStyle(color: Colors.grey),
+                                  ),
+                                  Text(
+                                    "You are a ios developer",
+                                    style: TextStyle(color: Colors.grey),
+                                  ),
+                                  Text(
+                                    "You are a ios developer",
+                                    style: TextStyle(color: Colors.grey),
+                                  ),
+                                  Text(
+                                    "You are a ios developer",
+                                    style: TextStyle(color: Colors.grey),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 30,)
                   ],
                 ),
               ),
             ),
     );
   }
+}
+
+Widget smallbuttons(BuildContext context, String text, Color backgroundcolor,
+    Color textcolor, bool border) {
+  return Expanded(
+    child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: backgroundcolor,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          side: BorderSide(color: border ? Colors.grey : Colors.transparent),
+        ),
+        onPressed: () {},
+        child: Text(
+          text,
+          style: TextStyle(color: textcolor, fontSize: 16),
+        )),
+  );
+}
+
+Widget workcontainer() {
+  return Container(
+    width: 300,
+    decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: const Color.fromARGB(255, 192, 187, 170)),
+    child: Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Open to work",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              const Text(
+                "IOS developer role",
+                style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.black,
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              GestureDetector(
+                  onTap: () {},
+                  child: const Text(
+                    "See all details",
+                    style: TextStyle(color: Colors.blue, fontSize: 12),
+                  ))
+            ],
+          ),
+          const Icon(
+            Icons.edit,
+            size: 20,
+          )
+        ],
+      ),
+    ),
+  );
+}
+
+Widget _dashboardStat(String value, String label) {
+  return Expanded(
+    child: Padding(
+      padding: const EdgeInsets.only(left: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(
+            value,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+              fontSize: 16,
+            ),
+          ),
+          const SizedBox(height: 4),
+          Text(
+            label,
+            style: const TextStyle(fontSize: 12),
+            textAlign: TextAlign.start,
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+Widget _verticalDivider() {
+  return const VerticalDivider(
+    color: Color.fromARGB(255, 158, 158, 158),
+    thickness: 1,
+    indent: 0,
+    endIndent: 0,
+    width: 5,
+  );
 }
--- lib/viewmodel/auth_view_model.dart
+++ lib/viewmodel/auth_view_model.dart
@@ -11,61 +11,60 @@ class AuthViewModel extends ChangeNotifier {
   void login(String email, String password, BuildContext context) {
     if (_dummyUsers.containsKey(email) && _dummyUsers[email] == password) {
       _user = UserModel(
-        email: email,
-        password: password,
-        name: email.split('@')[0],
-        headline: 'Professional',
-        location: 'San Francisco, CA',
-      );
+          email: email,
+          password: password,
+          name: email.split('@')[0],
+          headline: 'Freelance IOS Developer | UIKit | SwiftUI |',
+          location: 'Colombo, Srilanka',
+          work: "Lean transition solutions - lts",
+                    languages: "Talks about #swiftui, and#iosdevelopment",
+
+          followers: 4413,
+          connection: 500);
       notifyListeners();
       Navigator.pushReplacement(
           context, MaterialPageRoute(builder: (_) => const Mainpage()));
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
-        
-        const SnackBar(content: Text('Invalid email or password'),
-         behavior: SnackBarBehavior.floating,
+        const SnackBar(
+          content: Text('Invalid email or password'),
+          behavior: SnackBarBehavior.floating,
           margin: EdgeInsets.only(top: 10, left: 10, right: 10),
           duration: Duration(seconds: 3),
-        
         ),
       );
-
-
-
-
     }
   }
 
   void signup(String email, String password, BuildContext context) {
     if (_dummyUsers.containsKey(email)) {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Email already registered'),
-         behavior: SnackBarBehavior.floating,
+        const SnackBar(
+          content: Text('Email already registered'),
+          behavior: SnackBarBehavior.floating,
           margin: EdgeInsets.only(top: 10, left: 10, right: 10),
           duration: Duration(seconds: 3),
         ),
       );
     } else if (email.isNotEmpty && password.length >= 6) {
-      _dummyUsers[email] = password; 
+      _dummyUsers[email] = password;
       _user = UserModel(
-        email: email,
-        password: password,
-        name: email.split('@')[0],
-        headline: 'Freelance IOS Developer | UIKit | SwiftUI |',
-        location: 'Colombo, Srilanka',
-        work: "Lean transition solutions - lts",
-        followers: 4413,
-        connection: 500
-        
-      );
+          email: email,
+          password: password,
+          name: email.split('@')[0],
+          headline: 'Freelance IOS Developer | UIKit | SwiftUI |',
+          location: 'Colombo, Srilanka',
+          languages: "Talks about #swiftui, and#iosdevelopment",
+          work: "Lean transition solutions - lts",
+          followers: 4413,
+          connection: 500);
       notifyListeners();
       Navigator.pushReplacement(
           context, MaterialPageRoute(builder: (_) => const Mainpage()));
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Invalid email or password'),
-        
+        const SnackBar(
+          content: Text('Invalid email or password'),
         ),
       );
     }
